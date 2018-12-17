3ea1a36f80eb2eb92ba60faae4e9b938d22b3d3e
= <=>
2018-12-17 15:34:28 +0100

    added login.css to the project

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
  <head>
-    <title></title>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <form method="GET" action="process.php">
+        <h3>Sign In</h3>
         <input type="text" placeholder="Enter Username">
         <input type="password" placeholder="Enter Password">
         <input type="submit">
