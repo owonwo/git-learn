28b399b55ba222665852b08ae80cc02253f7e5b6
= <=>
2018-12-17 15:40:22 +0100

    added the registration page

--- register.php
+++ register.php
@@ -6,8 +6,10 @@
 </head>
 <body>
     <form method="GET" action="process.php">
-        <h3>Sign In</h3>
-        <p>Don't have an account? <a href="register.php">Sign Up</a></p>
+        <h3>Sign Up</h3>
+        <p>Already have an account? <a href="index.php">Sign In</a></p>
+        <input type="text" placeholder="First Name">
+        <input type="text" placeholder="Last Name">
         <input type="text" placeholder="Enter Username">
         <input type="password" placeholder="Enter Password">
         <input type="submit">
